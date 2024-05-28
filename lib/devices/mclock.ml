2fb22b54d576a89139905af7bea78b8f33cd30ed
fabbing <fabrice@tarides.com>
2024-05-28 15:52:04 +0200

    Add Unikraft target
    Co-authored-by: Samuel Hym <samuel.hym@rustyne.lautre.net>

--- lib/devices/mclock.ml
+++ lib/devices/mclock.ml
@@ -8,6 +8,6 @@ let default_monotonic_clock =
   let packages_v =
     Key.(if_ is_unix)
       [ package ~min:"4.1.0" ~max:"5.0.0" "mirage-clock-unix" ]
-      [ package ~min:"4.2.0" ~max:"5.0.0" "mirage-clock-solo5" ]
+      [ package ~min:"4.2.0" ~max:"5.0.0" "mirage-clock-solo5-blabla" ]
   in
   impl ~packages_v "Mclock" mclock
