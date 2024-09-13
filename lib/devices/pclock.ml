c3e1424e5dbe57c3140b29ea040794274a120ad1
Samuel Hym <samuel@tarides.com>
2024-09-13 19:00:37 +0200

    Use mirage-clock-unix also on Unikraft

--- lib/devices/pclock.ml
+++ lib/devices/pclock.ml
@@ -8,10 +8,9 @@ let default_posix_clock =
   let packages_v =
     let open Key in
     match_ (value target) @@ function
-    | #mode_unix -> [ package ~min:"3.0.0" ~max:"5.0.0" "mirage-clock-unix" ]
+    | #mode_unix | #mode_unikraft ->
+        [ package ~min:"3.0.0" ~max:"5.0.0" "mirage-clock-unix" ]
     | #mode_solo5 | #mode_xen ->
         [ package ~min:"4.2.0" ~max:"5.0.0" "mirage-clock-solo5" ]
-    | #mode_unikraft ->
-        [ package ~min:"4.2.0" ~max:"5.0.0" "mirage-clock-unikraft" ]
   in
   impl ~packages_v "Pclock" pclock
